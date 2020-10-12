739336d6192f5c446a74e9fa3bdda5c529b0b5ac
Naoki <65452992+k911672@users.noreply.github.com>
2020-10-12 22:32:02 +0900

    Reactを用いたToDoアプリです

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import Form from './Form'
+import List from './List'
+import shortid from 'shortid'
+
+const App = () => {
+    const [todos, setTodos] = useState([])
+
+    const addTodo = content => {
+        setTodos([
+            ...todos,
+            {
+                content,
+                id:shortid.generate()
+            }
+        ])
+    }
+
+
+    const deleteTodo = id => {
+        setTodos(todos.filter(todo => todo.id !== id))
+    }
+
+    return (
+        <>
+            <h1>Todo App</h1>
+            <Form addTodo={addTodo} />
+            <List todos={todos} deleteTodo={deleteTodo}/>
+        </>
+    )
+}
+
+export default App
